0f7cb68191b4b4eb2aa42a979f8122dab0c4ddf4
anamariagg <80848976+anamariagg@users.noreply.github.com>
2025-10-24 18:15:28 -0500

    cuarta

--- app/productos/page.tsx
+++ app/productos/page.tsx
@@ -102,7 +102,6 @@ export default function Page() {
               animarCarrito ? "animate-bounce" : ""
             }`}
           >
-            🛒
             {carrito.length > 0 && (
               <span className="absolute -top-2 -right-2 bg-red-600 text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full">
                 {carrito.length}
